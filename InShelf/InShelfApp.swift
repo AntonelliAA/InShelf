ef978f5618fb71ea0e8c604bf088ba8ca48e732b
AntonelliAA <anthony.anthony179@gmail.com>
2025-08-08 13:48:22 -0300

    Initial Commit

---
+++ InShelf/InShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  InShelfApp.swift
+//  InShelf
+//
+//  Created by Anthony Antonelli Andrade on 08/08/25.
+//
+
+import SwiftUI
+
+@main
+struct InShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
